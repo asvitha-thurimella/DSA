d19ed03260e4debcaaf6c86e72b6b494987ac888
asvitha-thurimella <asvithathurimella@gmail.com>
2025-07-25 19:05:56 +0530

    Time: 8 ms (32.30%) | Memory: 44.7 MB (40.75%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+    while(s.contains(part))
+   
+    {
+    
+       s= s.replaceFirst(part,"");
+    
+
+
+    }
+     return s;
+    }
+}
