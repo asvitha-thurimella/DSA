a067628b41b67430d61482f13abae74e4927a252
asvitha-thurimella <asvithathurimella@gmail.com>
2025-07-28 19:11:32 +0530

    Time: 7 ms (73.72%) | Memory: 42.9 MB (50.08%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+    int[] s1count=new int[26];
+    int[] s2count=new int[26];
+    if(s1.length()>s2.length())
+    {
+        return false;
+    }
+    for(int i=0;i<s1.length();i++)
+    {
+        s1count[s1.charAt(i) - 'a']++;
+        s2count[s2.charAt(i) - 'a']++;
+    }
+    if(matches(s1count,s2count))
+    {
+    return true;
+    }
+    for(int i=s1.length();i<s2.length();i++)
+    {
+     s2count[s2.charAt(i)-'a']++;
+     s2count[s2.charAt(i-s1.length())-'a']--;
+
+    
+    if(matches(s1count,s2count))
+    {
+    return true;
+    }
+    }
+    return false;
+    }
+    private boolean matches(int a[],int b[])
+    {
+    for(int i=0;i<26;i++)
+    {
+        if(a[i]!=b[i])
+        {
+            return false;
+        }
+    }
+    return true;
+    }
+    }
+
