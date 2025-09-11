6c9b0276fd26f44ee852cc38a796019e64e95245
asvitha-thurimella <asvithathurimella@gmail.com>
2025-09-11 21:21:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (8.02%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+    int n=matrix.length;
+    int m=matrix[0].length;
+    int temp;
+    for(int i=0;i<n;i++)
+    {
+    for(int j=i;j<m;j++)
+    {
+       temp=matrix[i][j];
+       matrix[i][j]=matrix[j][i];
+       matrix[j][i]=temp;
+    }
+    }
+    for(int i=0;i<n;i++)
+    {
+    for(int j=0;j<m/2;j++)
+    {
+        temp=matrix[i][j];
+        matrix[i][j]=matrix[i][n-1-j];
+        matrix[i][n-1-j]=temp;
+    }
+    }
+        
+    }
+}
