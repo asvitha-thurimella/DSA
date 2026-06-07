45329cea0528e8f598fa570c6c53b1ddd818ce3c
asvitha-thurimella <asvithathurimella@gmail.com>
2026-06-07 22:41:31 +0530

    Time: 17 ms (7.70%) | Memory: 134 MB (60.67%) - LeetSync

---
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+      final int[][] DIRS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int[][] ans = new int[m][n];
+    Arrays.stream(ans).forEach(A -> Arrays.fill(A, -1));
+    int x = 0; // the current x position
+    int y = 0; // the current y position
+    int d = 0;
+
+    for (ListNode curr = head; curr != null; curr = curr.next) {
+      ans[x][y] = curr.val;
+      if (x + DIRS[d][0] < 0 || x + DIRS[d][0] == m || y + DIRS[d][1] < 0 || //
+          y + DIRS[d][1] == n || ans[x + DIRS[d][0]][y + DIRS[d][1]] != -1)
+        d = (d + 1) % 4;
+      x += DIRS[d][0];
+      y += DIRS[d][1];
+    }
+
+    return ans;   
+    }
+}
