85b40ec88a76e937957a19220e294fc34a594d08
asvitha-thurimella <asvithathurimella@gmail.com>
2026-08-30 18:45:28 +0530

    Time: 2 ms (83.76%) | Memory: 41.9 MB (80.00%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,16 +1,21 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-    int original=num;
-    int sum=0;
+
+    int sum=1;
     if (num <= 1)
       return false;
-    for(int i=1;i<num;i++)
+    for(int i=2;i*i<=num;i++)
     {
         if(num%i==0)
         {
          sum+=i;
+        
+        if(i!=num/i)
+        {
+            sum+=num/i;
+        }
         }
     }
-    return original==sum;
+    return num==sum;
     }
 }
